221b2d7e74355520a9c7cb6b040ec25983594ab8
Paul Schifferer <paul@schifferers.net>
2022-09-12 08:08:35 -0700

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/sweetrpg_library_web"))
+sys.path.insert(0, os.path.abspath("../src/foreman"))
 
 
 # -- Project information -----------------------------------------------------
